8289e0137f9ab1cec9bd8142ff968d030f0972e6
Ghrf6 <111276076+Ghrf6@users.noreply.github.com>
2023-04-26 15:59:34 +0200

    schöner

--- 26.04.23_leo/app.js
+++ 26.04.23_leo/app.js
@@ -1,6 +1,6 @@
-let counter = 1
-function sayHi(){
-    counter++
-    alert("Guten Tag")
-    console.log("Wurde " + counter + " mal gedrückt")
+let counter = 1;
+function sayHi() {
+  counter++;
+  alert("Guten Tag");
+  console.log("Wurde " + counter + " mal gedrückt");
 }
--- 26.04.23_leo/premier/app.js
+++ 26.04.23_leo/premier/app.js
@@ -1,78 +1,74 @@
 if (window.location.href.includes("maps.html")) {
-    window.onload = function() {
-        fillMaps()
-    };
-  }
+  window.onload = function () {
+    fillMaps();
+  };
+}
 
-  if (window.location.href.includes("stats.html")) {
-    window.onload = function() {
-        fillPlayer()
-    };
-  }
+if (window.location.href.includes("stats.html")) {
+  window.onload = function () {
+    fillPlayer();
+  };
+}
 
-  
-  if (window.location.href.includes("weapons.html")) {
-    window.onload = function() {
-      fillWeapons()
-    };
-  }
-  
-  
+if (window.location.href.includes("weapons.html")) {
+  window.onload = function () {
+    fillWeapons();
+  };
+}
 
 function fillPlayer() {
-    fetch('data/player.json')
-      .then(response => response.json())
-      .then(data => {
-        for (let i = 0; i < data.Spieler.length; i++) {
-          let player = data.Spieler[i];
-          let row = document.getElementsByTagName("tr")[i+1];
-          row.cells[0].getElementsByTagName("input")[0].value = player.Name;
-          row.cells[1].getElementsByTagName("input")[0].value = player.Rolle;
-          row.cells[2].getElementsByTagName("input")[0].value = player['HS%'];
-          row.cells[3].getElementsByTagName("input")[0].value = player['Win%'];
-          row.cells[4].getElementsByTagName("input")[0].value = player['K/D'];
-        }
-      })
-      .catch(error => {
-        console.error('Fehler beim Laden der JSON-Datei:', error);
-      });
-  }
+  fetch("data/player.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.Spieler.length; i++) {
+        let player = data.Spieler[i];
+        let row = document.getElementsByTagName("tr")[i + 1];
+        row.cells[0].getElementsByTagName("input")[0].value = player.Name;
+        row.cells[1].getElementsByTagName("input")[0].value = player.Rolle;
+        row.cells[2].getElementsByTagName("input")[0].value = player["HS%"];
+        row.cells[3].getElementsByTagName("input")[0].value = player["Win%"];
+        row.cells[4].getElementsByTagName("input")[0].value = player["K/D"];
+      }
+    })
+    .catch((error) => {
+      console.error("Fehler beim Laden der JSON-Datei:", error);
+    });
+}
 
-  function fillMaps() {
-    fetch('data/maps.json')
-      .then(response => response.json())
-      .then(data => {
-        for (let i = 0; i < data.Maps.length; i++) {
-          let map = data.Maps[i];
-          let row = document.getElementsByTagName("tr")[i+1];
-          row.cells[0].getElementsByTagName("input")[0].value = map.Name;
-          row.cells[1].getElementsByTagName("input")[0].value = map['Win%'];
-          row.cells[2].getElementsByTagName("input")[0].value = map['AVG K/D'];
-          row.cells[3].getElementsByTagName("input")[0].value = map.wins
-          row.cells[4].getElementsByTagName("input")[0].value = map.losses;
-        }
-      })
-      .catch(error => {
-        console.error('Fehler beim Laden der JSON-Datei:', error);
-      });
-  }
-  
-  function fillWeapons() {
-    console.log("data weapons");
-    fetch('data/weapons.json')
-      .then(response => response.json())
-      .then(data => {
-        const figures = document.getElementsByTagName("figure");
-        for (let i = 0; i < data.Weapons.length && i < figures.length; i++) {
-          let gun = data.Weapons[i];
-          let figcaptionElements = figures[i].getElementsByTagName("figcaption");
-          figcaptionElements[0].textContent = gun.Name;
-          figcaptionElements[1].textContent = gun['HS%'];
-          figcaptionElements[2].textContent = gun['K/D'];
-        }
-      })
-      .catch(error => {
-        console.error('Fehler beim Laden der JSON-Datei:', error);
-      });
-  }
-  
+function fillMaps() {
+  fetch("data/maps.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.Maps.length; i++) {
+        let map = data.Maps[i];
+        let row = document.getElementsByTagName("tr")[i + 1];
+        row.cells[0].getElementsByTagName("input")[0].value = map.Name;
+        row.cells[1].getElementsByTagName("input")[0].value = map["Win%"];
+        row.cells[2].getElementsByTagName("input")[0].value = map["AVG K/D"];
+        row.cells[3].getElementsByTagName("input")[0].value = map.wins;
+        row.cells[4].getElementsByTagName("input")[0].value = map.losses;
+      }
+    })
+    .catch((error) => {
+      console.error("Fehler beim Laden der JSON-Datei:", error);
+    });
+}
+
+function fillWeapons() {
+  console.log("data weapons");
+  fetch("data/weapons.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const figures = document.getElementsByTagName("figure");
+      for (let i = 0; i < data.Weapons.length && i < figures.length; i++) {
+        let gun = data.Weapons[i];
+        let figcaptionElements = figures[i].getElementsByTagName("figcaption");
+        figcaptionElements[0].textContent = gun.Name;
+        figcaptionElements[1].textContent = gun["HS%"];
+        figcaptionElements[2].textContent = gun["K/D"];
+      }
+    })
+    .catch((error) => {
+      console.error("Fehler beim Laden der JSON-Datei:", error);
+    });
+}
